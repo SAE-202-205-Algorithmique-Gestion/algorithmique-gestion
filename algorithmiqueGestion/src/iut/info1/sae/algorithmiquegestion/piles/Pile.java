656898106f3038e13541dd73ac0bf9ba62bfafd5
Loïc Fgrs <faugieres.lolilolo@gmail.com>
2023-05-29 17:50:24 +0200

    Finition Pile.java et TestPile.java + PileVideException.java commencée

--- algorithmiqueGestion/src/iut/info1/sae/algorithmiquegestion/piles/Pile.java
+++ algorithmiqueGestion/src/iut/info1/sae/algorithmiquegestion/piles/Pile.java
@@ -5,69 +5,113 @@
 
 package iut.info1.sae.algorithmiquegestion.piles;
 
+import java.util.Arrays;
+
 /**
- * Classe de fabrication et de gestion de piles
- * Une pile fonctionne de la façon suivante : LIFO.
+ * Collection d'objets à loi de gestion LIFO = Last In First Out.
+ * <p>
+ * Classe non immuable (collection).
+ * 
  * @author Simon GUIRAUD
  * @author Loïc FAUGIERES
  */
 public class Pile {
+	
 	/**	Nombre max d'objets empilables initialement */
 	private static final int CAPACITE_INITIALE = 10;
 
 	/** Representation contigue = bloc de références des objets empilés */
 	private Object[] contenu;
 	
-	/** TODO : faire la javadoc */
+	/** Nombre effectif d'objets empilés */
 	private int taille;
 	
 	/**
-	 * TODO : faire la javadoc
+	 * Initialisation d'une pile vide et instanciation d'une
+	 * liste d'objets de taille CAPACITE_INITIALE.
 	 */
 	public Pile() {
 		super();
 		contenu = new Object[CAPACITE_INITIALE];
-		// taille = 0;
+		// taille = 0; // pile vide
 	}
 	
 	/**
-	 * TODO : faire la javadoc
-	 */
-	public Pile(Object element) {
-		contenu = new Object[1];
-		contenu[0] = element;
-		
-		
-	}
-	
-	/**
-	 * TODO : faire la javadoc
+	 * Prédicat vérifiant que cette pile ne référence aucun élément.
+	 * 
+	 * @return true si aucun élément empilé, false sinon.
 	 */
 	public boolean isVide() {
 		return taille == 0;
-		// TODO : compléter la méthode
 	}
 	
 	/**
-	 * Accesseur de l'attribut contenu
-	 * @return Attribut contenu
+	 * Ajoute un élément aAjouter au sommet de cette pile.
+	 * <p>
+	 * Commande modifiant l'état de this.</p>
+	 * 
+	 * @param aAjouter Référence de l'élément à empiler (non nulle).
+	 * @return Référence de cette pile après empilement.
+	 * @throws NullPointerException si aAjouter est null.
 	 */
-	public Object getContenu() {
-		return contenu;
-	}
-	/**
-	 * TODO : faire la javadoc
-	 */
-	public Pile empiler(Object element) {
-		Pile pileEmpile = new Pile();
-		return pileEmpile;
+	public Pile empiler(Object aAjouter) {
+		if (aAjouter == null) {
+			throw new NullPointerException("Impossible d'empiler la réf. null");
+		}
+		verifierTailleElements();
+		contenu[taille] = aAjouter;
+		taille++;
+		return this;
 	}
 	
 	/**
-	 * TODO : faire la javadoc
+	 * Retire de cette pile la référence du dernier élément
+	 * empilé en suivant la méthode LIFO (DAPS en français).
+	 * 
+	 * @return Cette pile après dépilement.
+	 * @throws PileVideException si cette pile est vide.
 	 */
-	public Pile depiler(Object element) {
-		Pile pileDepile = new Pile();
-		return pileDepile;
+	public Pile depiler() {
+		preConditionPileNonVide();
+		taille--;
+		return this;
 	}
+	
+	/**
+	 * Reallocation d'un nouveau tableau lorsque le tableau elements
+	 * est plein et recopie des anciens éléments.
+	 */
+	private void verifierTailleElements() {
+		final double GROSSISSEMENT = 1.25; // 25% de taille
+		if (taille == contenu.length) {
+			contenu = Arrays.copyOf(contenu,
+									(int) Math.ceil(taille * GROSSISSEMENT));
+		} else if (taille < contenu.length) {
+			contenu = Arrays.copyOf(contenu, taille + 1);
+		}
+	}
+	
+	/**
+	 * Accès au dernier empilé sur cette pile.
+	 * 
+	 * @return Référence du dernier élément empilé.
+	 * @throws PileVideException si cette pile est vide.
+	 */
+	public Object sommet() {
+		preConditionPileNonVide();
+		return contenu[taille - 1];
+	}
+	
+	/**
+	 * Vérifie une pré-condition non vide avant opération.
+	 * 
+	 * @throws PileVideException si pile vide.
+	 */
+	private void preConditionPileNonVide() throws PileVideException {
+		/* pré-condition : pile non vide */
+		if (isVide()) {
+			throw new PileVideException("Pré-condition pile non vide échoue");
+		}
+	}
+	
 }
--- algorithmiqueGestion/src/iut/info1/sae/algorithmiquegestion/piles/tests/TestPile.java
+++ algorithmiqueGestion/src/iut/info1/sae/algorithmiquegestion/piles/tests/TestPile.java
@@ -6,42 +6,106 @@
 package iut.info1.sae.algorithmiquegestion.piles.tests;
 
 import iut.info1.sae.algorithmiquegestion.piles.Pile;
+import iut.info1.sae.algorithmiquegestion.piles.PileVideException;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
+ * Classe de tests unitaires de la classe Pile
+ * (iut.info1.sae.algorithmiquegestion.piles.Pile).
  * 
- * @author Simon Guiraud
- * @author Loïc Faugières
+ * @author Simon GUIRAUD
+ * @author Loïc FAUGIERES
  */
 public class TestPile {
 	
+	/* fixture de test */
+	private Pile pileVide = new Pile();
+	
+	private Pile[] piles;
+	
 	/**
-	 * Méthode de test du constructeur de la classe Pile
+	 * Fixture de création de piles non vides pour les divers tests.
+	 */
+	@BeforeEach
+	void creerPilesNonVides() {
+		piles = new Pile[3];
+		
+		piles[0] = new Pile();
+		piles[0].empiler(Integer.valueOf(-3));
+		
+		piles[1] = new Pile();
+		for (int valeur = 1; valeur <= 50000; valeur++) {
+			final Integer nombre = Integer.valueOf(valeur);
+			piles[1].empiler(nombre);
+		}
+	}
+	
+	/**
+	 * Méthode de test de {@link iut.info1.sae.algorithmiquegestion.piles.Pile}.
 	 */
 	@Test
+	@DisplayName("Test du constructeur de la classe Pile")
 	void testPile() {
-		Pile pileTest = new Pile();
-		
-		assertTrue(pileTest.isVide());
+		assertTrue(pileVide.isVide());
 	}
 	
 	/**
-	 * Méthode de test de la méthode getContenu()
+	 * Méthode de test de
+	 * {@link iut.info1.sae.algorithmiquegestion.piles.Pile#isVide()}.
 	 */
 	@Test
-	void testGetContenu() {
-		Pile pileTest = new Pile(5);
-		
-		Object[] objetAttendu = new Object[5];
-		
-		assertEquals(objetAttendu, pileTest.getContenu());
+	@DisplayName("Test de la méthode isVide() de la classe Pile")
+	void testIsVide() {
+		assertTrue(pileVide.isVide());
+		assertFalse(piles[0].isVide());
+		assertFalse(piles[1].isVide());
 	}
 	
+	/**
+	 * Méthode de test de
+	 * {@link iut.info1.sae.algorithmiquegestion.piles.Pile#empiler()}.
+	 */
+	@Test
+	@DisplayName("Test de la méthode empiler() de la classe Pile")
+	void testEmpiler() {
+		assertThrows(NullPointerException.class,
+					 () -> piles[0].empiler(null));
+		
+		Pile grandePile = new Pile();
+		for (int valeur = 1; valeur < 50000; valeur++) {
+			final Integer nombre = Integer.valueOf(valeur);
+			assertDoesNotThrow(() -> grandePile.empiler(nombre));
+		}
+	}
+	
+	/**
+	 * Méthode de test de
+	 * {@link iut.info1.sae.algorithmiquegestion.piles.Pile#depiler()}.
+	 */
+	@Test
+	@DisplayName("Test de la méthode depiler() de la classe Pile")
+	void testDepiler() {
+		assertThrows(PileVideException.class, () -> pileVide.depiler());
+		
+		for (int valeur = 50000; valeur >= 1; valeur--) {
+			assertEquals(Integer.valueOf(valeur), piles[1].sommet());
+			piles[1].depiler();
+		}
+		assertTrue(piles[1].isVide());
+	}
+	
+	/**
+	 * Méthode de test de
+	 * {@link iut.info1.sae.algorithmiquegestion.piles.Pile#sommet()}.
+	 */
+	@Test
+	@DisplayName("Test de la méthode sommet() de la classe Pile")
+	void testSommet() {
+		assertThrows(PileVideException.class, () -> pileVide.sommet());
+		assertEquals(Integer.valueOf(-3), piles[0].sommet());
+	}
 }
