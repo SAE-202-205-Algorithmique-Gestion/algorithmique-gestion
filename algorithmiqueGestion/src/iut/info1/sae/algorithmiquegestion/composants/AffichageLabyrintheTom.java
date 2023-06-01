04eaff2d35b11fb9d466e48db5bd295beec665a7
Samuel.Lacam <SamuelLacam.S.lacam@outlook.fr>
2023-06-01 22:45:57 +0200

    ajout de message adresser a l'utilisateur (les meme que dans affichageLabyrinthe + commencement la réécriture du code pour quil soit plus propre

--- algorithmiqueGestion/src/iut/info1/sae/algorithmiquegestion/composants/AffichageLabyrintheTom.java
+++ algorithmiqueGestion/src/iut/info1/sae/algorithmiquegestion/composants/AffichageLabyrintheTom.java
@@ -5,22 +5,74 @@ import iut.info1.sae.algorithmiquegestion.jeuxlabyrinthe.Labyrinthe;
 // TODO el ravadocumentacion
 public class AffichageLabyrintheTom {
 	
-	public final static String MUR_VERTICALE = "---+";
-	
+	public final static String MUR_VERTICALE = "---";
+
 	public final static String LIAISON_HORIZONTALE = "   ";
 	
 	public final static String MUR_HORIZONTALE = " | ";
 	
-//	public final static String VIDE_VERTICAL = "   \u0000   ";
-	public final static String LIAISON_VERTICALE = "    ";
+	public final static String LIAISON_VERTICALE = "   ";
+	
+	private static final String MUR_BORDURE = "----";
+
+	private static final String COIN_DE_MUR = "+";
+
+	private static final char CASE = ' ';
+	
+	public final static String COMMANDES =
+			"""
+			- Z : déplacement vers le haut
+			- S : déplacement vers le bas
+			- Q : déplacement vers la gauche
+			- D : déplacement vers la droite
+			""";
+	
+	public final static String LANCEMENT_JEU =
+			"""
+			Bienvenue sur ce jeu de Labyrinthe !
+			
+			Veuillez d'abord renseigner la longueur et la largeur'
+			Voici la liste des commandes utilisables dans la console texte :
+			"""
+			+ COMMANDES +
+			"""
+				Tapez la touche entrée une fois que votre liste de commandes correspond à l'action souhaitée.
+				
+				Vous commencez sur la case E et devez atteindre la case S.
+				Votre position courante est représentée par X.
+				Si vous revenez sur la case E (l'entrée), X ne sera plus affiché.
+						
+						Vous devez vous déplacer là où une liaison apparaît.
+						Dans le cas contraire rien ne se passe.
+						""";
+	
 	
 	private final static String PARTIE_GAGNEE = "\nBravo !\nVous avez gagné, la"
 			+ " partie est terminée !";
+
+	
+
+    private final static String ERREUR_SAISIE =
+    		"""
+    		\nVous avez entré une commande inconnue !
+    		
+    		Voici la liste des commandes utilisables dans la console texte :
+    		"""
+    		+ COMMANDES;
+
+    private final static String DEMANDE_COMMANDE
+	= "\n\nEntrez votre/vos commande(s) : ";
+
+    private static final int nombreLignes = 5;
+    private static final int nombreColonnes = 5;
+
+	private static Labyrinthe labyrinthe = new Labyrinthe(nombreLignes, nombreColonnes);
+	
+	private static Sommet[] listeSommets = labyrinthe.getGraphe().getListeSommets();
 	
 	// TODO el ravadocumentacion
 	public static void main(String[] args) {
 		
-		Labyrinthe testSam = new Labyrinthe(5, 5);
 		
 //		for (int indexSommet = 0; indexSommet < testSam.getGraphe().getNombreSommets(); indexSommet++) {
 //			System.out.println("Sommet : " + testSam.getGraphe().getListeSommets()[indexSommet]);
@@ -30,82 +82,118 @@ public class AffichageLabyrintheTom {
 //			
 //		}
 		
-		int ligne;
+		System.out.println(LANCEMENT_JEU);
+		
+		int ligneCourante;
 		do {
-			ligne = 0;
-			System.out.print("+");
-			for (int j = 0; j < testSam.getGraphe().getNombreColonnesLabyrinthe(); j++) {
-				System.out.print(MUR_VERTICALE);
-			}
-
-			System.out.println();
+			ligneCourante = 0;
+			bordureHauteEtBasse();
 			
-			for (int i = 0; i < testSam.getGraphe().getNombreSommets(); i++) {
+			//Première bordure de gauche
+			System.out.print("\n| ");
+
+			for (int i = 0; i < listeSommets.length; i++) {
 				
-				if (testSam.getGraphe().getListeSommets()[i].getCoordonneeX() == 0) {
-					System.out.print("| ");
+				if (listeSommets[i].getCoordonneeX() == 0
+					&& listeSommets[i].getCoordonneeY() != 0) {
+					//Bordure latérale droite
+					System.out.print(" |");
 				}
 				
-				if (testSam.getGraphe().getListeSommets()[i].getCoordonneeY() == ligne + 1) {
-					System.out.print("|");
-					System.out.println("");
-					ligne++;
+ 				if (listeSommets[i].getCoordonneeY() == ligneCourante + 1) {
+ 					//Bordure latérale gauche
+					System.out.print("\n|");
 					
-					for (int rangLigne = 0; rangLigne < testSam.getGraphe().getNombreColonnesLabyrinthe(); rangLigne++) {
-						if (testSam.getGraphe().getListeSommets()[i + rangLigne]
-							.liaisonExiste
-							(testSam.getGraphe().getListeSommets()[i + rangLigne - testSam.getGraphe()
-																.getNombreColonnesLabyrinthe()])) {
-							
-							System.out.print (LIAISON_VERTICALE);
+					ligneCourante++;
+					
+					for (int rangLigne = 0; rangLigne < labyrinthe.getNombreDeColonne(); rangLigne++) {
+						if (affichageMursVertical(i, rangLigne)) {
+							System.out.print(LIAISON_VERTICALE);
 						} else {
 							System.out.print(MUR_VERTICALE);
 						}
-					}
-					System.out.println("");
-					
-				}
-				/* Affichage des sommets :
-				 * Entrée */
-				if (testSam.getEntree() == testSam.getGraphe().getListeSommets()[i]) {
-					System.out.print(testSam.getEntreeSymbole());
-					
-				/* Sortie */
-				} else if (testSam.getSortie() == testSam.getGraphe().getListeSommets()[i]) {
-					System.out.print(testSam.getSortieSymbole());
-					
-				/* Position actuelle */
-				} else if (testSam.getPositionActuelle() != testSam.getEntree()
-						   && testSam.getPositionActuelle() == testSam.getGraphe().getListeSommets()[i]) {
-					System.out.print(testSam.getSommetActuelleSymbole());
-				
-				/* les autres sommets */
-				} else {
-					System.out.print("#");
-				}
-					
-				if (i < testSam.getGraphe().getNombreSommets() - 1) {
-					if (testSam.getGraphe().getListeSommets()[i].getCoordonneeY() == testSam.getGraphe().getListeSommets()[i + 1].getCoordonneeY()) {
-						if (testSam.getGraphe().getListeSommets()[i].liaisonExiste(testSam.getGraphe().getListeSommets()[i + 1])) {
-							System.out.print(LIAISON_HORIZONTALE);
-						} else {
-							System.out.print(MUR_HORIZONTALE);
+						
+						if (rangLigne < labyrinthe.getNombreDeColonne() - 1) {
+							System.out.print(COIN_DE_MUR);
 						}
 					}
+					//Bordure latérale droite puis gauche
+					System.out.print("|" + "\n| "); 
+					
 				}
+ 				affichageSommets(i);
+ 				affichageMursHorizontaux(i);
 				
 			}
 			
-			System.out.println();
-
-			for (int j = 0; j < testSam.getGraphe().getNombreColonnesLabyrinthe(); j++) {
-                System.out.print(MUR_VERTICALE);
-            }
-
-			testSam.demandeDeplacement();
-			System.out.println("");
+			//dernière bordure latérale droite
+			System.out.println(" |");
 			
-		} while (testSam.getPositionActuelle() != testSam.getSortie());
+			bordureHauteEtBasse();
+
+			System.out.println(DEMANDE_COMMANDE);
+			if (!labyrinthe.demandeDeplacement()) {
+				System.out.print(ERREUR_SAISIE);
+			}
+			System.out.println();
+			
+		} while (labyrinthe.getPositionActuelle() != labyrinthe.getSortie());
 		System.out.println(PARTIE_GAGNEE);
+	}
+	
+	
+	
+	
+
+
+
+	private static boolean affichageMursVertical(int i, int rangLigne) {
+		if (listeSommets[i + rangLigne]
+			.liaisonExiste
+			(listeSommets[i + rangLigne - labyrinthe.getNombreDeColonne()])) {
+			return true;
+		}
+		return false;
+	}
+	
+	private static void bordureHauteEtBasse() {
+		System.out.print(COIN_DE_MUR);
+		for (int j = 0; j < labyrinthe.getGraphe().getNombreColonnesLabyrinthe() - 1; j++) {
+			System.out.print(MUR_BORDURE);
+		}
+		System.out.print("---+");
+	}
+	
+	private static void affichageSommets(int i) {
+		/* Entrée */
+		if (labyrinthe.getEntree() == listeSommets[i]) {
+			System.out.print(labyrinthe.getEntreeSymbole());
+			
+		/* Sortie */
+		} else if (labyrinthe.getSortie() == listeSommets[i]) {
+			System.out.print(labyrinthe.getSortieSymbole());
+			
+		/* Position actuelle */
+		} else if (labyrinthe.getPositionActuelle() != labyrinthe.getEntree()
+				   && labyrinthe.getPositionActuelle() == listeSommets[i]) {
+			System.out.print(labyrinthe.getSommetActuelleSymbole());
+		
+		/* les autres sommets */
+		} else {
+			System.out.print(CASE);
+		}
+	}
+	
+	private static void affichageMursHorizontaux(int i) {
+		if (i < labyrinthe.getGraphe().getNombreSommets() - 1
+			&& listeSommets[i].getCoordonneeY() == listeSommets[i + 1].getCoordonneeY()) {
+			
+			if (listeSommets[i].liaisonExiste(listeSommets[i + 1])) {
+				System.out.print(LIAISON_HORIZONTALE);
+			} else {
+				System.out.print(MUR_HORIZONTALE);
+			}
+		}
+		
 	}
 }
